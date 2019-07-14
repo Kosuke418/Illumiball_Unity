cb9c7e8d983edec5d17f93e3d88051789e8b6de8
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-07-15 01:46:33 +0900

    Holeとクリアチェッカーの設置

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    bool fallIn;
+    public string activeTag;
+
+
+    public bool IsFallIn()
+    {
+        return fallIn;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag==activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == activeTag)
+        {
+            fallIn = false;
+        }
+    }
+    // Start is called before the first frame update
+    void OnTriggerStay (Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+        if(other.gameObject.tag==activeTag)
+        {
+            r.velocity *= 0.9f;
+
+            r.AddForce(direction * r.mass * 20.0f);
+        }
+        else
+        {
+            r.AddForce(-direction * r.mass * 80.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/HoleRandom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleRandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(-4.0f, 4.0f), 1, Random.Range(-8.0f, 8.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
