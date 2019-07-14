80201c16d5c0bc32a93663448157818fd75001da
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-07-15 00:58:20 +0900

    ランダム配置で壁に当たらないように

--- Assets/Scripts/Randoms.cs
+++ Assets/Scripts/Randoms.cs
@@ -7,7 +7,7 @@ public class Randoms : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(Random.Range(-4.5f,4.5f),0, Random.Range(-8.5f, 8.5f));
+        transform.position = new Vector3(Random.Range(-4.0f,4.0f),0, Random.Range(-8.0f, 8.0f));
     }
 
     // Update is called once per frame
