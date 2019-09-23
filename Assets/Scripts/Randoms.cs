5021bd3ce1c81c99564f69c9763083e4b30e9e1b
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-09-23 18:01:49 +0900

    a

--- Assets/Scripts/Randoms.cs
+++ Assets/Scripts/Randoms.cs
@@ -10,9 +10,4 @@ public class Randoms : MonoBehaviour
         transform.position = new Vector3(Random.Range(-4.0f,4.0f),0, Random.Range(-8.0f, 8.0f));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/TimeCount.cs
+++ Assets/Scripts/TimeCount.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoleRandom : MonoBehaviour
+public class TimeCount : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(Random.Range(-4.0f, 4.0f), 1, Random.Range(-8.0f, 8.0f));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
